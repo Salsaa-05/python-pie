1762ba0e6f334a5b96a94d608710828fcb7b18a7
Salsaa-05 <saadhamouda95@gmail.com>
2023-10-28 18:25:12 +0200

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+Primo = input("what whould you like to eat for first? " )
+print(Primo + " good choice")
+secondo = input("And for your main dish ,sir! " )
+print("awesoome!")
+Drink = input("And for the drink? " )
+print(Drink + " perfect")
+
